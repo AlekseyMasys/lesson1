dd36b8de9a202384c2a01236061f5b8d4cb4b4f0
Aleksey Masys <masis.leha@mail.ru>
2020-02-16 15:59:52 +0700

    12

--- src/ru/masis/ClassForCherryPick.java
+++ src/ru/masis/ClassForCherryPick.java
@@ -2,6 +2,6 @@ package ru.masis;
 
 public class ClassForCherryPick {
     public static void main(String[] args) {
-        System.out.println("from branch 5");
+        System.out.println("from branch");
     }
 }
