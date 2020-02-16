eed1effe066a1f0680f7a4a5ec86f96e68d6226d
Aleksey Masys <masis.leha@mail.ru>
2020-02-16 18:50:34 +0700

    master ch

--- src/ru/masis/Lesson1.java
+++ src/ru/masis/Lesson1.java
@@ -2,7 +2,7 @@ package ru.masis;
 
 public class Lesson1 {
     public static void main(String[] args) {
-        System.out.println("fro");
+        System.out.println("master changes");
     }
 
 }
