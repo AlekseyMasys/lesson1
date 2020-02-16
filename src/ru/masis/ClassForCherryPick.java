0cf774fecf611daaafa509731d5b93ee9186fc1d
Aleksey Masys <masis.leha@mail.ru>
2020-02-16 16:03:08 +0700

    Revert "12"
    This reverts commit dd36b8de

--- src/ru/masis/ClassForCherryPick.java
+++ src/ru/masis/ClassForCherryPick.java
@@ -2,6 +2,6 @@ package ru.masis;
 
 public class ClassForCherryPick {
     public static void main(String[] args) {
-        System.out.println("from branch");
+        System.out.println("from branch 5");
     }
 }
