04ebb6b651f3034eb80844b8d3a118a6cae7f2c9
Aleksey Masys <masis.leha@mail.ru>
2020-03-03 23:40:08 +0700

    Revert "cherry-pic1"
    This reverts commit 80bea3a3

--- src/ru/masis/Lesson1.java
+++ src/ru/masis/Lesson1.java
@@ -2,9 +2,9 @@ package ru.masis;
 
 public class Lesson1 {
     public static void main(String[] args) {
+        System.out.println("hello from childdev1");
         System.out.println("Hello dev");
         System.out.println("cherry-pick1");
-        System.out.println("dont cherry-pick");
         System.out.println("First comit from dev");
         System.out.println("second com after childdev1created");
     }
