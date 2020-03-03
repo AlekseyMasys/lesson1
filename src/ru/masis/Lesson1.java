60053b8d24d1a5d5f3aedda1644bd3fdfea5bdfe
Aleksey Masys <masis.leha@mail.ru>
2020-03-03 22:14:46 +0700

    second com after childdev1created

--- src/ru/masis/Lesson1.java
+++ src/ru/masis/Lesson1.java
@@ -4,6 +4,7 @@ public class Lesson1 {
     public static void main(String[] args) {
         System.out.println("Hello dev");
         System.out.println("First comit from dev");
+        System.out.println("second com after childdev1created");
     }
 
 }
