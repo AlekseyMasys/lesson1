500370fc56c0ab0660cc50da988efe8b76c17f8f
Aleksey Masys <masis.leha@mail.ru>
2020-03-04 01:14:17 +0700

    Revert "amend2"
    This reverts commit 85e9bb2d

--- src/ru/masis/Lesson1.java
+++ src/ru/masis/Lesson1.java
@@ -2,10 +2,8 @@ package ru.masis;
 
 public class Lesson1 {
     public static void main(String[] args) {
-        System.out.println("hello from childdev1");
-        System.out.println("нрннрн");
-        System.out.println("свсвс com after childdev1created");
-        //ddfdfdfdfdfvdvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvdddddddddddsdsdsdsdsdsds
+        System.out.println("Hello dev");
+        System.out.println("ппипи");
     }
 
 }
