80bea3a3530075ae3559300d11238e41dffb768e
Aleksey Masys <masis.leha@mail.ru>
2020-03-03 23:23:03 +0700

    cherry-pic1

--- src/ru/masis/Lesson1.java
+++ src/ru/masis/Lesson1.java
@@ -2,8 +2,8 @@ package ru.masis;
 
 public class Lesson1 {
     public static void main(String[] args) {
-        System.out.println("hello from childdev1");
         System.out.println("Hello dev");
+        System.out.println("cherry-pick1");
         System.out.println("First comit from dev");
         System.out.println("second com after childdev1created");
     }
