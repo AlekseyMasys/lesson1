85e9bb2da31f61a33e0df78b3b4c487c5e87fc9d
Aleksey Masys <masis.leha@mail.ru>
2020-03-04 00:05:02 +0700

    amend2

--- src/ru/masis/Lesson1.java
+++ src/ru/masis/Lesson1.java
@@ -2,8 +2,10 @@ package ru.masis;
 
 public class Lesson1 {
     public static void main(String[] args) {
-        System.out.println("Hello dev");
-        System.out.println("ппипи");
+        System.out.println("hello from childdev1");
+        System.out.println("нрннрн");
+        System.out.println("свсвс com after childdev1created");
+        //ddfdfdfdfdfvdvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvddddddddddd
     }
 
 }
