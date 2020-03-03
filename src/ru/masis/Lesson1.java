77a534dd7f053dd5862fffe2afbf7e7eaf250e77
Aleksey Masys <masis.leha@mail.ru>
2020-03-03 22:12:07 +0700

    first com fr dev

--- src/ru/masis/Lesson1.java
+++ src/ru/masis/Lesson1.java
@@ -2,6 +2,7 @@ package ru.masis;
 
 public class Lesson1 {
     public static void main(String[] args) {
+        System.out.println("hello from childdev1");
         System.out.println("Hello dev");
         System.out.println("First comit from dev");
         System.out.println("second com after childdev1created");
